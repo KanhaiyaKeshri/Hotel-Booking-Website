7cec0a5ba72495c5b09e03e39c849aa67b448f92
KanhaiyaKeshri <kanhaiyakeshri101@gmail.com>
2024-01-21 14:52:41 +0530

    Deleting Photos and selecting main cover photo functionality added

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -46,6 +46,12 @@ function PlacesPage() {
             >
               <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                 {/* <PlaceImg place={place} /> */}
+                {place.photos.length > 0 && (
+                  <img
+                    src={`http://localhost:3000/uploads/${place.photos[0]}`}
+                    alt=""
+                  />
+                )}
               </div>
               <div className="grow-0 shrink">
                 <h2 className="text-xl">{place.title}</h2>
